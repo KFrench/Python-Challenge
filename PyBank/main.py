bf9aee658c2e0b9e60bc13f99a9ddc1125eec2a0
Kaleah French <kaleah.french@gmail.com>
2020-01-06 07:31:05 -0500

    Added code to calculate average change

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,27 +2,46 @@
 import csv
 import os
 import statistics 
-#Create Variables for calculations
+
+#Create lists for calculations
 Count_Months=[]
 Count_Profit=[]
 Aver_Change=[]
+
 #set up csv file to import
-budget_data = os.path.join("..","Python-Challenge-master","PyBank","Resources","budget_data.csv")
-#print("I ran")
+budget_data = os.path.join("Resources","budget_data.csv")
+
 #Open csv and read csv
-with open("/Users/test/Desktop/Python-Challenge-master/PyBank/Resources/budget_data.csv", "r") as New:
+with open(budget_data, "r") as New:
     csv_reader=csv.reader(New)
+   
    #skip the headers 
     next(csv_reader) 
-    #for line in csv_reader: 
-       #print(line)
-    #this loop will create list for total number of months and profit/losses to be calculated
-    #Counter=0
+   
+   #Create variables for calculating average change
+    cur_month = None  
+    pre_month = None 
+    total_change = []
+
+   #Create loop for reading through each row in the csv file
     for row in csv_reader:
         Count_Months.append(row[0])
         Count_Profit.append(int(row[1]))
-        #Counter=+-1
-        #print(len(Counter))
-    print(f"Total Number of Months{Count_Months}")
-    print(f"Total Profit/Loss:{len(Count_Profit)}")
-  
+    print(f"Total Number of Months:{len(Count_Months)}")
+    
+    # Sum numeric values for profit/loss
+    Sum_Count_Profit=sum(Count_Profit)
+    print(f"Total Profit/Loss:${Sum_Count_Profit}")
+   
+    #find the average change 
+        if cur_month is None: 
+            cur_month = int(row[1])
+            pre_month = int(row[1])
+        else:
+            cur_month = int(row[1])
+            total_change.append(cur_month - pre_month)
+    Average_Change=sum(total_change)/len(total_change)
+    print(f"Average Change:${Average_Change}")
+   
+    #Find greatest increase 
+   
